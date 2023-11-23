edf613e62be41ed39edc4030e260b6e1e5169f12
daxstermoore <admin@daxster.ca>
2023-11-23 06:27:55 -0800

    Completed Q3

--- lab3.s
+++ lab3.s
@@ -6,9 +6,15 @@
     K:		.SPACE 2
 	   
     .TEXT
-    .GLOBAL
+    .GLOBAL	__reset
     
 __reset:
+    MOV		#0xC7, W0
+    MOV		#0x25, W1
+    MOV		W0, 0X820
+    MOV		W1, 0x822
+    ADD		W0,W1,W2
+    MOV		W2, 0x840
     
 HERE: GOTO HERE
 
